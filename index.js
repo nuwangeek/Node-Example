d9ba0e05016821d226685f53ed9bf174674d48f6
nuwangeek <charithym321@gmail.com>
2021-02-21 10:22:23 -0800

    node callbacks and error handling

--- index.js
+++ index.js
@@ -2,15 +2,20 @@
 var rect = require('./rectangle');//this is how rectangle node module be called
 
 function solveRect(l,b){
-    console.log("solving for rectangle with l = "+l + "and b =" + b);
-
-    if (l<=0 || b<=0){
-        console.log("Rectangle dimensions should be grater than zero. l =" + l + "and b=" + b);
-
-    }else{
-        console.log("Area of rectangle = "+rect.area(l,b));
-        console.log("perimeter of rectangle = " +rect.perimeter(l,b));
-    }
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
 }
 
 solveRect(2,3);
--- rectangle.js
+++ rectangle.js
@@ -1,4 +1,20 @@
 //This is a node module
+//Here we use setTimeout for simulating the delay of the function
 
-exports.perimeter = (x,y) => (2*(x+y));
-exports.area = (x,y) => (x*y);
+module.exports = (x,y,callback) => {
+    if (x <= 0 || y <= 0)
+        setTimeout(() => 
+            callback(new Error("Rectangle dimensions should be greater than zero: l = "
+                + x + ", and b = " + y), 
+            null),
+            2000);
+    else
+        //here javascript object be passed into the secend property of call back function
+        setTimeout(() => 
+            callback(null, {
+                //perimeter function can accesed x,y so dont need to put again 
+                perimeter: () => (2*(x+y)),
+                area:() => (x*y)
+            }), 
+            2000);
+}
